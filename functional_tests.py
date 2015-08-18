bcf23c8f728f89de9ebf9fd55ee8ab495b82dcaa
Marcus Krahl <mail@marcuskrahl.de>
2015-08-18 21:01:49 +0200

    functional test now checks we can create a new diary entry

--- functional_tests.py
+++ functional_tests.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 import unittest
+from selenium.webdriver.common.keys import Keys
 
 class NewJournalEntryTest(unittest.TestCase):
     
@@ -16,15 +17,25 @@ class NewJournalEntryTest(unittest.TestCase):
 
         # She notices the page title and header mention the daily journal
         self.assertIn('Daily Journal', self.browser.title)
-        self.fail('Finish the test!')
-        
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('Daily Journal', header_text)
+
         # She has the option to enter her journal entry
-        
+        inputbox = self.browser.find_element_by_id('id_new_journal_entry')
+        self.assertEqual(inputbox.get_attribute('placeholder'),'Enter a new journal entry')
+
         # She types her journal entry
+        inputbox.send_keys('This is my brand new diary entry')
         
-        # When she confirms the entry, the page updates and her entry is shown as commited
+        # When she sends the entry, the page updates and her entry is shown as commited
+        send_button = self.browser.find_element_by_id('id_submit_journal_entry')
+        send_button.click()
+        
+        entries = self.browser.find_elements_by_css('p.entry')
+        self.assertTrue(any(entry.text == 'This is my brand new diary entry' for entry in entries))
         
         # She is refreshing the page, her entry is still there
+        self.fail("Finish test")
 
         # Satisfied, she closes the browser
 
