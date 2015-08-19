9b40edb1e67ee1d59dd1c12068b6b829b080eb10
Marcus Krahl <mail@marcuskrahl.de>
2015-08-19 21:23:48 +0200

    Model for journal entries and associated migration

--- journal/tests.py
+++ journal/tests.py
@@ -3,6 +3,7 @@ from django.test import TestCase
 from journal.views import home_page
 from django.http import HttpRequest
 from django.template.loader import render_to_string
+from journal.models import JournalEntry
 
 class HomePageTest(TestCase):
 
@@ -26,3 +27,21 @@ class HomePageTest(TestCase):
         expected_html = render_to_string('home.html', {'journal_entry_text': 'A new journal entry'})
         self.assertEqual(response.content.decode(), expected_html)
 
+class JournalEntryModelTest(TestCase):
+
+    def test_saving_and_retrieving_journal_entries(self):
+        first_journal_entry = JournalEntry()
+        first_journal_entry.text = 'The first (ever) journal entry'
+        first_journal_entry.save()
+
+        second_journal_entry = JournalEntry()
+        second_journal_entry.text = 'Second journal entry'
+        second_journal_entry.save()
+
+        saved_journal_entries = JournalEntry.objects.all()
+        self.assertEqual(saved_journal_entries.count(),2)
+
+        first_journal_entry = saved_journal_entries[0]
+        second_journal_entry = saved_journal_entries[1]
+        self.assertEqual(first_journal_entry.text,'The first (ever) journal entry')
+        self.assertEqual(second_journal_entry.text,'Second journal entry')
