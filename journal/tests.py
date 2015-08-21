802762fc860ab6e1f3a491abc642866e67ff58f9
Marcus Krahl <mail@marcuskrahl.de>
2015-08-21 20:34:17 +0000

    simplified test for template usage in home page

--- journal/tests.py
+++ journal/tests.py
@@ -12,10 +12,8 @@ class HomePageTest(TestCase):
         self.assertEqual(found.func, home_page)
 
     def test_home_page_returns_correct_html(self):
-        request = HttpRequest()
-        response = home_page(request)
-        expected_html = render_to_string('home.html')
-        self.assertEqual(response.content.decode(),expected_html)
+        response = self.client.get('/')
+        self.assertTemplateUsed(response,'home.html')
 
     def test_home_page_can_save_a_POST_request(self):
         response = self.client.post('/',{'entry_text':'A new journal entry'})
